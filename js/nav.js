9bf47dc9467032059da2e69cb17765f93a8d9132
Victor Hernandez <v.hernandez@cgiar.org>
2023-01-16 16:19:05 -0500

    cambio logo de alianza por el de aeps

--- js/nav.js
+++ js/nav.js
@@ -1,42 +1,18 @@
 window.addEventListener("DOMContentLoaded", (event) => {
   // Navbar shrink function
   var navbarShrink = function () {
-    const navbarCollapsible = document.body.querySelector("#mainNav");
+    const navbarCollapsible = document.body.querySelector("#logo-aeps");
+    console.log(navbarCollapsible)
     if (!navbarCollapsible) {
       return;
     }
     if (window.scrollY === 0) {
-      navbarCollapsible.classList.remove("fixed-top");
+      navbarCollapsible.classList.remove("logo-aeps-p");
     } else {
-      navbarCollapsible.classList.add("fixed-top");
+      navbarCollapsible.classList.add("logo-aeps-p");
     }
   };
 
-  // Shrink the navbar
-  navbarShrink();
-
   // Shrink the navbar when page is scrolled
   document.addEventListener("scroll", navbarShrink);
-
-  // Activate Bootstrap scrollspy on the main nav element
-  const mainNav = document.body.querySelector("#mainNav");
-  if (mainNav) {
-    new bootstrap.ScrollSpy(document.body, {
-      target: "#mainNav",
-      offset: 74,
-    });
-  }
-
-  // Collapse responsive navbar when toggler is visible
-  const navbarToggler = document.body.querySelector(".navbar-toggler");
-  const responsiveNavItems = [].slice.call(
-    document.querySelectorAll("#navbarResponsive .nav-link")
-  );
-  responsiveNavItems.map(function (responsiveNavItem) {
-    responsiveNavItem.addEventListener("click", () => {
-      if (window.getComputedStyle(navbarToggler).display !== "none") {
-        navbarToggler.click();
-      }
-    });
-  });
 });
